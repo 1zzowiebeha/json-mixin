7aee401799faf4a9d885750f42685eb9c7f21018
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2023-08-27 14:07:05 -0400

    Implement JSerializerMixin
    Fixes the previous iteration. Now it works!! Huzzahh!!

--- jmixin.py
+++ jmixin.py
@@ -2,64 +2,40 @@ import json
 from inspect import signature
 
 
-class SerializerMixin: # mixin should not have conflicting __init__ method
+class JSerializerMixin: # mixin should not have conflicting __init__ method
     def to_json(self):
         # todo: ensure security by checking annotations
         return json.dumps(self.__dict__)
 
     @classmethod
-    def from_json(cls, json_string, *args):
-        new_class: type(cls)
+    def from_json(cls, json_string):
+        new_class: type(cls) # uninitialized var of type cls
         json_object = json.loads(json_string)
 
         # Does cls have an __init__ method?
         if hasattr(cls, '__init__') and callable( getattr(cls, '__init__') ):
             init_signature = signature(cls.__init__)
-            new_class = cls(**init_signature.parameters) # initialize our object with keyword args
+            init_params = dict(init_signature.parameters) # keys are param names. values are just Parameter types (we will not use) 
+
+            # create a set from deserialized instance attrs that doesn't include init params
+            # this will allow us to not set the same init instance attrs twice
+            json_without_signature = set(json_object.items()) - set(init_params.items())
+
+            del init_params['self'] # omit self from arguments to be initialized
             
-            # take right venn diagram so we don't set the same variables again
-            json_object = json_object ^ init_signature
-             
-        # set unset instance methods:
+            # set init param argument values to actual deserialized values (not Parameter type values)
+            for key in init_params.keys():
+                init_params[key] = json_object[key]
+
+            # finally, initialize out new object with init_params
+            new_class = cls(**init_params)
+            
+            # premature/minute optimization? measure performance with/without and check
+            json_object = dict(json_without_signature)
+        
+        # set instance attributes that aren't in __init_: 
         # improve performance for this with vars() maybe?
         for key, value in json_object.items():
             new_class.__dict__[key] = value
         
         return new_class
-
-
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __repr__(self):
-        string = f"{type(self).__name__}("
-        
-        for key, value in self.__dict__.items():
-            string += f"{key} = {value},"
-
-        string = ")"
-
-
-class Employee(SerializerMixin, Person):
-    def __init__(self, name, age, salary):
-        super().__init__(name, age) # init person
-        self.salary = salary
-        self._spooky = True
-        
-        self.to_json() # must reference self
-        
-    def greeting(self):
-        print("hello!")
-
-
-def main():
-    employee = Employee('Jogn', 44, 12000)
-    employee2 = Employee.from_json(employee.to_json())
-    
-    employee2.greeting()
-
-    
-if __name__ == "__main__":
-    main()
